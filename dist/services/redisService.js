3205600f2313060219f3f38638e7b69772e1367c
adgupta-1 <21bec029@iiitdmj.ac.in>
2025-01-31 01:22:01 +0530

    'second-commit'

--- dist/services/redisService.js
+++ dist/services/redisService.js
@@ -1,9 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const ioredis_1 = require("ioredis");
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
 const redisClient = new ioredis_1.Redis({
-    host: 'redis-12185.c11.us-east-1-3.ec2.redns.redis-cloud.com',
-    port: 12185,
-    password: 'PfFyNYFHE5Dax7SU4pOBWSY31zh0k3E9'
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 12185,
+    password: process.env.REDIS_PASSWORD,
 });
 exports.default = redisClient;
--- src/services/redisService.ts
+++ src/services/redisService.ts
@@ -1,9 +1,11 @@
-import {Redis} from 'ioredis'
+import { Redis } from "ioredis";
+import { config } from "dotenv";
+config();
 
 const redisClient = new Redis({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 12185,
-    password: process.env.REDIS_PASSWORD
-})
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 12185,
+  password: process.env.REDIS_PASSWORD,
+});
 
 export default redisClient;
